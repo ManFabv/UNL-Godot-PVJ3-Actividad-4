6f6508f5bc51755e99572f43dd6ff9e5a5532f42
Fabricio Manrique <fabemanrique@hotmail.com>
2022-06-05 16:32:55 -0300

    Created apply_charge shader param. Set shader param names as constants in script

--- player/player.gd
+++ player/player.gd
@@ -7,21 +7,25 @@ export var weapon_3_tint : Color;
 
 onready var sprite_material = get_material();
 
+const APPLY_TINT_SHADER_VAR = "apply_tint"
+const TINT_COLOR_SHADER_VAR = "tint_color"
+const APPLY_CHARGE_SHADER_VAR = "apply_charge"
+
 
 func _input(event):
 	if(event.is_action_pressed("select_weapon_1")):
-		sprite_material.set_shader_param("apply_tint", true)
-		sprite_material.set_shader_param("tint_color", weapon_1_tint)
+		sprite_material.set_shader_param(APPLY_TINT_SHADER_VAR, true)
+		sprite_material.set_shader_param(TINT_COLOR_SHADER_VAR, weapon_1_tint)
 	elif(event.is_action_pressed("select_weapon_2")):
-		sprite_material.set_shader_param("apply_tint", true)
-		sprite_material.set_shader_param("tint_color", weapon_2_tint)
+		sprite_material.set_shader_param(APPLY_TINT_SHADER_VAR, true)
+		sprite_material.set_shader_param(TINT_COLOR_SHADER_VAR, weapon_2_tint)
 	elif(event.is_action_pressed("select_weapon_3")):
-		sprite_material.set_shader_param("apply_tint", true)
-		sprite_material.set_shader_param("tint_color", weapon_3_tint)
+		sprite_material.set_shader_param(APPLY_TINT_SHADER_VAR, true)
+		sprite_material.set_shader_param(TINT_COLOR_SHADER_VAR, weapon_3_tint)
 	elif(event.is_action_pressed("select_weapon_default")):
-		sprite_material.set_shader_param("apply_tint", false)
+		sprite_material.set_shader_param(APPLY_TINT_SHADER_VAR, false)
 	
 	if(event.is_action_pressed("select_weapon_charge")):
-		print("space pressed")
+		sprite_material.set_shader_param(APPLY_CHARGE_SHADER_VAR, true)
 	elif(event.is_action_released("select_weapon_charge")):
-		print("space released")
+		sprite_material.set_shader_param(APPLY_CHARGE_SHADER_VAR, false)
